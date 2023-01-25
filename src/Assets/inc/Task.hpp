23ebd1dc9178dfbb713f67a0f9074f06b723e4df
David <david.bnicolae@gmail.com>
2023-01-25 10:32:35 +0200

    Added Task base class

---
+++ src/Assets/inc/Task.hpp
@@ -0,0 +1,52 @@
+// Copyright © 2022 David Bogdan
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
+// (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+// so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+/**
+ * @file Task.hpp
+ * @author David Bogdan (david.bnicolae@gmail.com)
+ * @brief Definition of the Task abstract class
+ * @version 0.1
+ * @date 2023-01-20
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+#pragma once
+
+namespace easyGUI
+{
+
+/**
+ * @brief Wrapper around an abstract callable.
+ * 
+ * @details This class serves to provide an interface for other
+ * classes which need to run any function with any parameters.
+ * This way, any derived class will be compatible with methods
+ * such as onClick() and onHover()
+ * 
+ * @warning This class cannot be used as a standalone.
+ */
+class Task
+{
+public:
+    /**
+     * @brief Executes the task
+     * 
+     */
+    virtual void exec();
+};
+
+};
