381faf2cf1cc7b531fca4eeaf8940500d6854146
David Bogdan <david.bnicolae@gmail.com>
2023-09-10 11:32:11 +0300

    Fixed cpplint license

--- src/Application/inc/Exceptions/ApplicationException.hpp
+++ src/Application/inc/Exceptions/ApplicationException.hpp
@@ -1,16 +1,19 @@
 // Copyright © 2022 David Bogdan
 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files 
-// (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, 
-// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do 
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the “Software”), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do
 // so, subject to the following conditions:
 
-// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in all copies or substantial
+// portions of the Software.
 
-// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE 
-// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT
+// LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 /**
@@ -40,19 +43,14 @@
 #define INFO std::cout << "[INFO] "
 #endif
 
-namespace easyGUI
-{
+namespace easyGUI {
 
 /**
  * @brief Exception thrown when something goes wrong at Application level.
  * 
  */
-class ApplicationException : public ::std::exception
-{
-private:
-    ::std::string _msg;
-
-public:
+class ApplicationException : public ::std::exception {
+ public:
     /**
      * @brief Destructor
      * 
@@ -72,10 +70,12 @@ public:
      * 
      * @return const char* 
      */
-    const char* what() const noexcept
-    {
+    const char* what() const noexcept {
         return _msg.c_str();
-    }   
+    }
+
+ private:
+    ::std::string _msg;
 };
 
-}
+}  // namespace easyGUI
